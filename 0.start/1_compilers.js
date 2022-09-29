fee928892e2ef8ff7bdddc6b92fdf058ff76274c
sachin kumar <sachinmehtab@gmail.com>
2022-09-30 01:46:10 +0530

    started js from scratch for bunty

---
+++ 0.start/1_compilers.js
@@ -0,0 +1,8 @@
+//check compiler versions
+
+// javac --version // for java
+// g++ --version  // for c++
+// gcc --version   // cor c
+// python --version  //for python
+// node --version    // for javascript
+// javascript intrtpretter (compiler) is also present inside chrome (v8 engine)
---
+++ 0.start/2_compileAndRun.js
@@ -0,0 +1,7 @@
+// compile and run a file
+
+// g++ filename.cpp
+// javac filename.java
+// node filenme.js (node .\2_compileAndRun.js)
+// use tab dont write your self use tabs to auto complete
+// ctrl +l  is use to clear any terminal
