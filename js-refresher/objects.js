68df05cb351d6eac2329dfc9cac531d85040d5d1
sachin kumar <sachinmehtab@gmail.com>
2022-10-07 23:34:50 +0530

    sorting proj

--- js-refresher/objects.js
+++ js-refresher/objects.js
@@ -9,11 +9,14 @@ let sachin={
 
 sachin["pno"]=283964;
 
-// console.log(JSON.stringify(sachin));
+console.log(JSON.stringify(sachin));
 // console.log(sachin.fname);
 // console.log(sachin.lname);
 // console.log(sachin.age);
 // sachin.getdetails();
 
 // console.log(sachin.fname,sachin.age,sachin.lname,sachin.getdetails);
-console.log(sachin);
+// console.log(sachin);
+
+let myjson=`{"fname":"sachin","lname":"kumar","age":21,"pno":283964}`;
+console.log(JSON.parse(myjson).fname);
