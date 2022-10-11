6c152876d99d9c83e24af37339a375db085aca6c
sachin kumar <sachinmehtab@gmail.com>
2022-10-11 19:36:11 +0530

    organised

--- higher order function/higher_order_direct_func.cpp
+++ higher order function/higher_order_direct_func.cpp
@@ -16,13 +16,23 @@ vector<double>calculate(vector<int> radius,double (*logic)(int k)){
 int main(){
    
    vector<int> radius={42,6,8,19,4};
-   vector<double> ans=calculate(radius, *[](int k){return M_PI*k*k;});
+   vector<double> ans_area=calculate(radius, *[](int k){return 3.1415926*k*k;});
+   vector<double> ans_circumference=calculate(radius, *[](int k){return 3.1415926*2*k;});
+   vector<double> ans_diameter=calculate(radius, *[](int k){return 2.0*k;});
 
    /*
     #define _USE_MATH_DEFINES
     #include<cmath>
    */
-   for(auto item:ans){
+   for(auto item:ans_area){
+    cout<<item<< '\t'; 
+   }
+    cout<<endl;
+   for(auto item:ans_circumference){
+    cout<<item<< '\t'; 
+   }
+    cout<<endl;
+   for(auto item:ans_diameter){
     cout<<item<< '\t'; 
    }
     cout<<endl;
